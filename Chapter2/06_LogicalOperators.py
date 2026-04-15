24b83dab006e38551c39c347f318e43aadaaecfd
mohdnabee <mohdnabeel989@gmail.com>
2026-04-15 10:30:50 +0530

    Logical  Operators

---
+++ Chapter2/06_LogicalOperators.py
@@ -0,0 +1,26 @@
+# Logical Operators
+# e = True or False
+# print(e)
+
+#   Truth Table for OR
+print ("True or False is", True or False)
+print ("True or True is", True or True)
+print("False or True is", False or True)
+print ("False or False is", False or False)
+
+
+#   Truth Table for AND
+print()
+print ("True and False is", True and False)
+print ("True and True is", True and True)   
+print("False and True is", False and True)
+print ("False and False is", False and False)
+
+
+#   Truth Table for NOT
+print()
+print ("Not True is", not True)
+print ("Not False is", not False)
+
+print (not(False))
+print (not(True))
---
+++ Chapter2/07_type.py
@@ -0,0 +1,13 @@
+# a = 31
+# a = 31.2
+a= "Nabeel"
+t= type (a) # class <int> 
+print (t) 
+
+b =  31 
+print (type(b))
+
+c =  "31.2"
+cc = float(c) # c but the type should be float 
+cc1 =  type(cc)
+print (cc1) 
