f20c693dafb177b6ecb9e62703da3e21597cadf6
mohdnabee <mohdnabeel989@gmail.com>
2026-04-20 22:05:57 +0530

    Dictionary

---
+++ Chapter5/01_Dic.py
@@ -0,0 +1,11 @@
+
+marks = {
+    "Nabeel" : 100, 
+    "Shubham" :90 , 
+    "Rohan" : 80 ,  
+    "list" :[1,2,9]
+}
+
+# print (marks ,  type(marks))
+print (marks["Nabeel"])
+print (marks["list"])
---
+++ Chapter5/02_Dict.py
@@ -0,0 +1,17 @@
+marks = {
+    "Nabeel" : 100, 
+    "Shubham" :90 , 
+    "Rohan" : 80 ,  
+    0 : "Harry"
+}
+
+# print (marks.items())
+#  print (marks.keys())
+# print (marks.values())  
+
+# marks.update({"Nabeel": 99 ,"Renuka" : 100})
+# print (marks)
+
+
+print (marks.get("Nabeel2"))#  prints none 
+print(marks["Nabeel2"])# returs error 
