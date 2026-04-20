feb8b60fdb590ea2b4e77c92f8571971b7644e98
mohdnabee <mohdnabeel989@gmail.com>
2026-04-20 18:52:39 +0530

    tuples

---
+++ Chapter4/03_tuples.py
@@ -0,0 +1,8 @@
+a =  (1, 2 , 5 ,6)
+b = () # empty tuple
+c =  (1,) # tuple with single element .  A comma is required after the element in a single element tuple .
+d= (1, 45 , 342 , 3424 , False , "Nabeel" , "Shivam")
+
+print (type(a)) # tuple is a collection which is ordered and unchangeable .  Tuples are written with round brackets .
+print (d)
+1
---
+++ Chapter4/04_tuples_Methods.py
@@ -0,0 +1,10 @@
+a =  (1, 45 , 342 , 3424 ,45 , False , "Nabeel" , "Shivam")
+print(a)
+
+no = a.count (45)
+print (no) # count method is used to count the number of occurrences of an element in the tuple .
+
+i =  a.index (3424)
+print(i) #index method is used to find the index of the first occurrence of an element in the tuple .
+
+print (len(a)) # len function is used to find the number of elements in the tuple .
