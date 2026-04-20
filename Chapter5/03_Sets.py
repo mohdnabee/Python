88536c9bb189b27ad2b99af97eac5270490211b5
mohdnabee <mohdnabeel989@gmail.com>
2026-04-21 00:05:47 +0530

    Sets

---
+++ Chapter5/03_Sets.py
@@ -0,0 +1,5 @@
+e = set () # empty Set   Dont use s = {} as it will create an empty dictionary not a set
+
+s =  {1, 5 , 32, 54 , 5 ,5 ,5}
+print(s)
+
---
+++ Chapter5/04_Sets.py
@@ -0,0 +1,13 @@
+s =  { 1, 5 , 32 , 54 , 5 ,5 ,5 , "Harry" }
+
+print (s,type(s))
+
+s.add(566)
+print (s)
+
+s.remove(1)
+print (s, type(s))
+
+
+
+
