d0216d6baf538c9a25d0308dbe1523fd5f675880
mohdnabee <mohdnabeel989@gmail.com>
2026-04-20 13:23:47 +0530

    lists

---
+++ Chapter4/02_List_methods.py
@@ -0,0 +1,28 @@
+friends = ["Apple","Orange",5, 345.06, False,"Aakash", "Rohan"]
+
+print(friends)
+
+friends.append("Nabeel") #  append method is used to add an element at the end of the list
+print(friends)
+
+
+l1 = [1, 34 , 5 , 11,2 , 62, 6]
+# l1.sort() # sort method is used to sort the list in ascending order
+
+# l1.reverse() # reverse method is used to reverse the list   
+
+# l1.insert (3,333) # insert method is used to insert an element at a specific index in the list . 
+# Insert  333 such that  its index in the list is 3 .  
+
+
+# l1.pop(3) # pop method is used to remove an element from the list at a specific index .  
+
+# print(l1.pop(3)) # pop method also returns the element that is removed from the list .
+
+# value = l1.pop(3)
+# print(value) # 333 is removed from the list and returned by the pop method .
+
+
+l1.remove(11) # remove method is used to remove the first occurrence of an element from the list .
+
+print (l1)
