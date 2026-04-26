e9f1f39297564ddc1b0b64b585897d79a0f1191d
mohdnabee <mohdnabeel989@gmail.com>
2026-04-26 10:44:43 +0530

    recursion

---
+++ Chapter8/05_recursion.py
@@ -0,0 +1,7 @@
+def factorial(n) :
+    if(n==1 or n==0):
+     return 1
+    return n * factorial(n -1)
+
+n =  int(input("Enter The Number:-  "))
+print(f"The Factorial of this number is:- {factorial(n)} ")
